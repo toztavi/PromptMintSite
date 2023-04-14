551e3febb72b63bda9452635c2e81c0c1e704dfd
tavi_ai <toztavi@imedia-tec.com>
2023-04-15 03:44:15 +0900

    Add Ownable

--- contracts/PromptNFT.sol
+++ contracts/PromptNFT.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.2 <0.9.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts@4.6.0/access/Ownable.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Strings.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
 
-contract PromptNFT is ERC1155 {
+contract PromptNFT is ERC1155, Ownable {
     using Strings for uint256;
 
     string private _baseURI = "https://api.nft.storage/";
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
 
     constructor() ERC1155(_baseURI) {
     }
@@ -16,11 +21,14 @@ contract PromptNFT is ERC1155 {
         return string(abi.encodePacked(_baseURI, tokenId.toString()));
     }
 
-    function mint(address to, uint256 id, uint256 amount) public {
-        _mint(to, id, amount, "");
+    function mint(uint256 amount) public onlyOwner {
+        _tokenIds.increment();
+        uint256 newTokenId = _tokenIds.current();
+
+        _mint(_msgSender(), newTokenId, amount, "");
     }
 
-    function setBaseURI(string memory newBaseURI) public {
+    function setBaseURI(string memory newBaseURI) public onlyOwner {
         _baseURI = newBaseURI;
     }
 }
