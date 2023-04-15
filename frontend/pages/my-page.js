e2d4ee2a72b7515a80975a1409682a1cf57b1b57
tavi_ai <77526451+toztavi@users.noreply.github.com>
2023-04-15 09:53:03 +0900

    add mypage.js

--- frontend/pages/my-page.js
+++ frontend/pages/my-page.js
@@ -1,23 +1,18 @@
 import Head from 'next/head';
 import Link from 'next/link';
 
-const Minted = () => {
+const MyPage = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
       <Head>
-        <title>Minted</title>
+        <title>My Page</title>
       </Head>
-      <h1 className="text-4xl font-bold mb-8">Minted</h1>
-      <p className="text-xl mb-8">The generated number is unique and minted.</p>
+      <h1 className="text-4xl font-bold mb-8">My Page</h1>
+      <p className="text-xl mb-8">Welcome to My Page!</p>
       <div className="mt-8">
         <Link href="/" passHref>
           <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow">
             Go back to index
-          </button>
-        </Link>
-        <Link href="/my-page" passHref>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-4">
-            My Page
           </button>
         </Link>
       </div>
@@ -25,4 +20,4 @@ const Minted = () => {
   );
 };
 
-export default Minted;
+export default MyPage;
